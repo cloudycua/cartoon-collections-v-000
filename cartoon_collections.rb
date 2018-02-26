af86c1c10ed7165d0316d58de0acecd1c8909f55
Christine Cua <cloudycua@gmail.com>
2018-02-26 17:10:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(names)
   list = []
-  names.each.with_index(1).map do |dwarf, index|
+  names.each_with_index(1).map do |dwarf, index|
     list << "#{index}. *#{dwarf}"
   end
   puts list
